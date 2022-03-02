3790460c676c8f7acd0bc2e96d2b6d5f74c3c99f
DUBUKE <94811647+DUBUKE@users.noreply.github.com>
2022-03-02 16:03:22 +0900

    0302

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 use App\Models\User;
 
+use Admin;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,6 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(10)->create();
+        // User::factory(10)->create();
+        $this->call([
+            AdminSeeder::class,
+            UserSeeder::class, 
+            ArticleSeeder::class,
+        ]);
     }
 }
